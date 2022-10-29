b2daf6c00f7871a898121024e21ed9d0ba7b7065
MarcelaMoran7 <moranmarce777@gmail.com>
2022-10-29 12:16:27 -0600

    Commit ya hice unos cambios en el main layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
-  <q-layout view="hHh Lpr lFf">
-    <q-header elevated class="bg-yellow">
+  <q-layout view="hHh Lpr lFf" class="bg-grey-4">
+    <q-header elevated class="bg-yellow-5">
       <q-toolbar>
         <q-btn
           flat
@@ -18,7 +18,19 @@
           Tienda CellPhone
         </q-toolbar-title>
 
-        <div></div>
+        <q-input class="GPL__toolbar-input" dense standout="bg-grey" v-model="search" placeholder="Search">
+          <template v-slot:prepend>
+            <q-icon v-if="search === ''" name="search" />
+            <q-icon v-else name="clear" class="cursor-pointer" @click="search = ''" />
+          </template>
+        </q-input>
+        <q-space />
+
+        <q-btn color="brown-5 q-m-auto" label="Inicio" />
+        <q-space />
+
+        <q-btn  round color="grey q-ma-sm" icon="las la-plus" />
+        <div><q-btn flat round color="dark q-ma-sm" icon="shopping_cart" size="20px"/></div>
       </q-toolbar>
     </q-header>
 
@@ -117,3 +129,41 @@ export default defineComponent({
   }
 })
 </script>
+<style lang="sass">
+.GPL
+
+  &__toolbar
+    height: 64px
+
+  &__toolbar-input
+    width: 35%
+    border-radius: 3rem
+  &__drawer-item
+    line-height: 24px
+    border-radius: 0 24px 24px 0
+    margin-right: 12px
+    .q-item__section--avatar
+      padding-left: 12px
+      .q-icon
+        color: #5f6368
+    .q-item__label:not(.q-item__label--caption)
+      color: #3c4043
+      letter-spacing: .01785714em
+      font-size: .875rem
+      font-weight: 500
+      line-height: 1.25rem
+    &--storage
+      border-radius: 0
+      margin-right: 0
+      padding-top: 24px
+      padding-bottom: 24px
+  &__side-btn
+    &__label
+      font-size: 12px
+      line-height: 24px
+      letter-spacing: .01785714em
+      font-weight: 500
+  @media (min-width: 1024px)
+    &__page-container
+      padding-left: 94px
+</style>
