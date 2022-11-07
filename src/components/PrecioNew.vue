e43d5ea2d0169fc02024eb4b501d4b2c8684479a
MarcelaMoran7 <moranmarce777@gmail.com>
2022-11-07 09:23:47 -0600

    commit solo le agregue unos espacios a algunos elementos

--- src/components/PrecioNew.vue
+++ src/components/PrecioNew.vue
@@ -3,16 +3,12 @@
 
     <legend>PRECIO</legend>
 
-    <q-input filled bottom-slots v-model="text" label="Ingrese el precio" label-color="yellow-9" counter :dense="dense">
+    <q-input filled bottom-slots v-model="text" label="Ingrese el precio" label-color="yellow-9"  :dense="dense">
       <template v-slot:prepend>
         <q-icon name="las la-dollar-sign" />
       </template>
       <template v-slot:append>
         <q-icon name="close" @click="text = ''" class="cursor-pointer" />
-      </template>
-
-      <template v-slot:hint>
-        Field hint
       </template>
       </q-input>
 
